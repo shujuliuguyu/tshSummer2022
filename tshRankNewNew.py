92a9c3a841118373bdd2d3a74af85d3ed39340f3
gudui <hua5679@gmail.com>
2022-07-21 18:07:57 +0800

    Simplify the code

--- tshRankNewNew.py
+++ tshRankNewNew.py
@@ -5,17 +5,9 @@ r2 = requests.get(
     "https://api.live.bilibili.com/activity/v1/Template/getRankList?act_id=2125&assist_num=5&rank_type=master&sub_type=gold_all&page=2&page_size=20")
 d1 = r1.json()
 d2 = r2.json()
+dAll=d1['data']['list']+d2['data']['list']
 
-for xoxData in d1['data']['list']:
-    print(str(xoxData['rank'])+" " +
-          xoxData['uname']+" " +
-          str(xoxData['score'])+" " ,end="")
-    for userData in xoxData['assist_list']:
-        print(userData['uname']+" " +
-        str(userData['score'])+" ",end="")
-    print()
-
-for xoxData in d2['data']['list']:
+for xoxData in dAll:
     print(str(xoxData['rank'])+" " +
           xoxData['uname']+" " +
           str(xoxData['score'])+" " ,end="")
@@ -25,10 +17,7 @@ for xoxData in d2['data']['list']:
     print()
 
 sum = 0
-for xoxData in d1['data']['list']:
-    sum += xoxData['score']
-
-for xoxData in d2['data']['list']:
+for xoxData in dAll:
     sum += xoxData['score']
 
 print("总收入："+str(sum))
