e580845388f6bc9769508acaf499bd17a3e75d27
gudui <hua5679@gmail.com>
2022-07-23 22:32:30 +0800

    add create timestamp

--- tshRankNewNew.py
+++ tshRankNewNew.py
@@ -1,19 +1,20 @@
 import requests
+import time
 r1 = requests.get(
     "https://api.live.bilibili.com/activity/v1/Template/getRankList?act_id=2125&assist_num=5&rank_type=master&sub_type=gold_all&page=1&page_size=20")
 r2 = requests.get(
     "https://api.live.bilibili.com/activity/v1/Template/getRankList?act_id=2125&assist_num=5&rank_type=master&sub_type=gold_all&page=2&page_size=20")
 d1 = r1.json()
 d2 = r2.json()
-dAll=d1['data']['list']+d2['data']['list']
+dAll = d1['data']['list']+d2['data']['list']
 
 for xoxData in dAll:
     print(str(xoxData['rank'])+" " +
           xoxData['uname']+" " +
-          str(xoxData['score'])+" " ,end="")
+          str(xoxData['score'])+" ", end="")
     for userData in xoxData['assist_list']:
         print(userData['uname']+" " +
-        str(userData['score'])+" ",end="")
+              str(userData['score'])+" ", end="")
     print()
 
 sum = 0
@@ -21,3 +22,4 @@ for xoxData in dAll:
     sum += xoxData['score']
 
 print("总收入："+str(sum))
+print("制表时间："+time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
--- tshUserRankNew.py
+++ tshUserRankNew.py
@@ -1,4 +1,5 @@
 import requests
+import time
 url1 = "https://api.live.bilibili.com/activity/v1/Template/getRankList?act_id=2125&assist_num=1&rank_type=user&sub_type=gold_all&page="
 url2 = "&page_size=20"
 rList = []
@@ -11,13 +12,14 @@ dList = []
 for i in range(0, 10):
     dList.append(rList[i].json())
 
-sum=0
+sum = 0
 
 for i in range(0, 10):
     for j in range(0, 20):
         print(str(dList[i]['data']['list'][j]['rank'])+" " +
               dList[i]['data']['list'][j]['uname']+" " +
               str(dList[i]['data']['list'][j]['score']))
-        sum+=dList[i]['data']['list'][j]['score']
+        sum += dList[i]['data']['list'][j]['score']
 
 print("Top200贡献总计："+str(sum))
+print("制表时间："+time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
